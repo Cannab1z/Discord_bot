bdec75aea6ba1c34767d467007a3d1a043430ebf
root <root@DESKTOP-CF3OJUF.localdomain>
2021-07-21 19:32:56 +0300

    added random disconnect role, trolling yaari and comer

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
-var afk_users = [];
+const afk_users = [];
 
 client.setInterval(() => {
   afk_users.forEach((user)=>{
-    console.log(user.member.nickname);
     var date = new Date();
     var mins = (date - user.time) / (1000 *60 );
-    console.log(mins);
+    console.log(`user ${user.member.nickname} was in afk ${mins} mins`);
     if(mins >= 5)
     {
       if(user.channel.channelID == user.member.guild.afkChannelID)
       {
         user.member.voice.kick();
         console.log(`kicked ${user.member.nickname}`);
-        //console.log(`kicked ${user}`);
         afk_users.shift();
         return;
       }
@@ -40,7 +38,29 @@ client.on('voiceStateUpdate', (oldMember, newMember) =>{
     console.log(`${newMember.member.nickname} moved to afk`);
     afk_users.push({member: newMember.member, time: new Date(), channel: newMember});
   }
+  else if(newMember.channelID != oldMember.channelID)
+  {
+    let special_role_users = newMember.member.guild.roles.cache.find(role => role.name === 'Gever').members.map(m=>m.user.tag);
+    special_role_users.forEach((user) => {
+      if(user === newMember.member.user.tag)
+      {
+        let number = Math.floor(Math.random() * 100);
+        console.log(number);
+        if(number < 5)
+        {
+          console.log(`kicked ${newMember.member.nickname}`)
+          newMember.member.voice.kick();
+          if(newMember.member.id === '253928420227022848')
+          {
+            let channel = newMember.member.guild.channels.cache.filter(channel => channel.type == "text");
+            channel.first().send(`${newMember.member.guild.members.cache.find(member => member.id == '197459744208715776').nickname} kicked ${newMember.member.nickname}`);
+          }
+        }
+      }
+    });
+  }
 });
+
 
 /*client.on('message', (message) => {
   //var guild = new Discord.Guild();
